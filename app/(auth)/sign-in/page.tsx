3cef33f61ea33c12204aa0de83fa384687f1bce7
ismail <esmael449@stud.fci-cu.edu.eg>
2024-12-29 20:34:28 +0200

    Authentecation setup, Added Github and Google Auth and added the sign in and sign up forms, pages,  and layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
 
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInShema } from "@/lib/validations";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      defaultValues={{ email: "", passord: "" }}
+      schema={SignInShema}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignIn;
