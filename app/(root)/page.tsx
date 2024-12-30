7402bc23a780b142976f3c6880e76c28e62d26a7
ismail <esmael449@stud.fci-cu.edu.eg>
2024-12-30 21:52:11 +0200

    Added the MobileNavigation component and the template for the pages in it

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,9 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-
 const Home = async () => {
-  const session = await auth();
-
   return (
     <>
       <h1 className="font-space-grotesk text-3xl font-black text-light-500">
         Welcome to the world of Next.js (Space Grotesk)
       </h1>
-      {session && (
-        <form
-          className="px-10 pt-[100px]"
-          action={async () => {
-            "use server";
-            await signOut({ redirectTo: ROUTES.SIGNIN });
-          }}
-        >
-          <Button type="submit">Log Out</Button>
-        </form>
-      )}
     </>
   );
 };
---
+++ components/navigation/navbar/NavLinks.tsx
@@ -0,0 +1,68 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+import { SheetClose } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+
+type NavLinksProps = {
+  isMobileNav?: boolean;
+};
+const NavLinks = ({ isMobileNav = false }: NavLinksProps) => {
+  const pathname = usePathname();
+  const userId = 1;
+  return (
+    <div className="flex flex-col gap-3">
+      {sidebarLinks.map((link) => {
+        const isActive =
+          (pathname.includes(link.route) && link.route.length > 1) ||
+          link.route === pathname;
+
+        if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
+        const LinkComponent = (
+          <Link
+            href={link.route}
+            key={link.label}
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-light-900"
+                : "text-dark300_light900",
+              "flex items-center justify-start gap-4 bg-transparent p-4"
+            )}
+          >
+            <Image
+              src={link.imgURL}
+              width={20}
+              height={20}
+              alt={link.label}
+              className={cn({
+                "invert-colors": !isActive,
+              })}
+            />
+            <p
+              className={cn(
+                isActive ? "base-bold" : "base-medium",
+                !isMobileNav && "max-lg:hidden"
+              )}
+            >
+              {link.label}
+            </p>
+          </Link>
+        );
+        return isMobileNav ? (
+          <SheetClose asChild key={link.route}>
+            {LinkComponent}
+          </SheetClose>
+        ) : (
+          <React.Fragment key={link.route}>{LinkComponent}</React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
