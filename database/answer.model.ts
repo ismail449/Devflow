77772d06685488b4dcb903939a8c13ae146444b3
ismail <esmael449@stud.fci-cu.edu.eg>
2025-03-07 19:30:28 +0200

    Add the Answer model

---
+++ database/answer.model.ts
@@ -0,0 +1,28 @@
+import { model, models, Types, Schema } from "mongoose";
+
+export interface IAnswer {
+  questionId: Types.ObjectId;
+  content: string;
+  authorId: Types.ObjectId;
+  upVotes?: number;
+  downVotes?: number;
+}
+
+const questionSchema = new Schema<IAnswer>(
+  {
+    questionId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Question",
+    },
+    content: { type: String, required: true },
+    authorId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Answer = models.Answer || model<IAnswer>("Answer", questionSchema);
+
+export default Answer;
