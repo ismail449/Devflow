c0627a1faf7989061aaeef3fd355a6ad50bd45de
ismail <esmael449@stud.fci-cu.edu.eg>
2025-03-15 14:24:51 +0200

    Add missing db connection in the account/provider route

--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import { AccountSchema } from "@/lib/validations";
 
 export async function POST(request: Request): Promise<Response> {
@@ -14,6 +15,7 @@ export async function POST(request: Request): Promise<Response> {
     if (!validatedData.success) {
       throw new ValidationError(validatedData.error.flatten().fieldErrors);
     }
+    await dbConnect();
     const account = await Account.findOne({ providerAccountId });
     if (!account) {
       throw new NotFoundError("Account");
