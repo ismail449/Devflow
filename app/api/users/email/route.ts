c01048174446bc93cf55e9d2404d74189217fb07
ismail <esmael449@stud.fci-cu.edu.eg>
2025-03-15 14:10:48 +0200

    Add database connection in email route handler

--- app/api/users/email/route.ts
+++ app/api/users/email/route.ts
@@ -3,11 +3,13 @@ import { NextResponse } from "next/server";
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import { UserSchema } from "@/lib/validations";
 
 export async function POST(request: Request): Promise<Response> {
   try {
     const { email } = await request.json();
+    await dbConnect();
     const validatedData = UserSchema.partial().safeParse({ email });
     if (!validatedData.success) {
       throw new ValidationError(validatedData.error.flatten().fieldErrors);
