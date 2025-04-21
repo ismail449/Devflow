421359c626c0830943e33cb2302cfd7ac4b0aff7
ismail <esmael449@stud.fci-cu.edu.eg>
2025-04-21 16:29:48 +0200

    Implement getTags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Tags = () => {
-  return <div>Tags</div>;
+import DataRenderer from "@/components/DataRenderer";
+import { EMPTY_TAGS } from "@/constants/states";
+import { getTags } from "@/lib/actions/tag.actions";
+
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    query: "",
+    page: 1,
+    pageSize: 10,
+  });
+  const { tags } = data || {};
+  return (
+    <DataRenderer
+      empty={EMPTY_TAGS}
+      render={(tags) => tags.map((tag) => <div key={tag._id}>{tag.name}</div>)}
+      success={success}
+      data={tags}
+      error={error}
+    />
+  );
 };
 
 export default Tags;
