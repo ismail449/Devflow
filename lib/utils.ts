8b6a0ff3a69a6bcef3a53ec61f03bee2970742a9
ismail <esmael449@stud.fci-cu.edu.eg>
2025-04-28 21:42:47 +0300

    Add formatNumber util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -75,3 +75,13 @@ export function getTimeStamp(createdAt: Date): string {
 
   return "just now";
 }
+
+export const formatNumber = (number: number) => {
+  if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + "M";
+  } else if (number >= 1000) {
+    return (number / 1000).toFixed(1) + "K";
+  } else {
+    return number.toString();
+  }
+};
