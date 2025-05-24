ad0f51658cac3ccc82e84a22f4b03d251af12e10
ismail <esmael449@stud.fci-cu.edu.eg>
2025-05-24 20:01:09 +0300

    Implement collections page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_QUESTION } from "@/constants/states";
+import { getSavedQuestions } from "@/lib/actions/collection.action";
 
-const Collection = () => {
-  return <div>Collection</div>;
+type SearchParams = Promise<{ [key: string]: string | undefined }>;
+
+const Collections = async ({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) => {
+  const { query = "", filter = "", page, pageSize } = await searchParams;
+
+  const { success, data, error } = await getSavedQuestions({
+    filter: filter || "",
+    query: query || "",
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+  });
+
+  const { collection: collections } = data || {};
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          placeholder="Search for Questions Here..."
+          imgSrc="/icons/search.svg"
+          otherClasses="flex-1"
+          route={ROUTES.COLLECTION}
+        />
+      </div>
+      <DataRenderer
+        success={success}
+        error={error}
+        data={collections}
+        empty={EMPTY_QUESTION}
+        render={(collections) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {collections.map((collection) => (
+              <QuestionCard
+                key={collection.question._id}
+                question={collection.question}
+              />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
 };
 
-export default Collection;
+export default Collections;
